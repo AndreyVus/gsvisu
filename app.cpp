d913a3593a387db93c2924b1dea90dace36d9f87
AndreyVus <62697596+AndreyVus@users.noreply.github.com>
2025-03-03 13:12:35 +0100

    v2

---
+++ app.cpp
@@ -0,0 +1,73 @@
+#include <UserCEvents.h>
+#include <UserCAPI.h>
+#include <gsAudio.h>
+#include "vartab.h"
+#include "objtab.h"
+#include "gstovisu.h"
+
+#include "..\..\lib.cpp"
+IPFunctions ipFunctions;
+
+#define TasteAnzahl 3
+#define TasteInhalt 105
+#define dimmTimer 60000
+
+class Maske0 : public Maske
+{
+private:
+public:
+    Maske0(){}
+    void cycle(uint32_t evtc, tUserCEvt *evtv) override
+	{
+    }
+    void timer() override
+	{
+	}
+};
+
+
+class Application
+{
+private:
+    std::vector<std::unique_ptr<Maske>> masken;
+    uint32_t curPage;
+public:
+    Application()
+    {
+        masken.push_back(std::make_unique<Maske0>()); // Maske0 hinzufügen
+        
+        SetBuzzer(1, 1, 1, 1);
+    }
+    void Cycle(uint32_t evtc, tUserCEvt *evtv)
+    {
+        menu = keymenu(evtc, evtv, TasteAnzahl, TasteInhalt, dimmTimer);
+        curPage = GetCurrentMaskShown();
+        masken[curPage]->cycle(evtc, evtv);
+    }
+    void Timer(void)
+    {
+        ipFunctions.ProcessFunctions();
+        masken[curPage]->timer();
+    }
+};
+Application *application;
+extern "C"
+{
+    int UserCPPInit(uint32_t initFlags)
+    {
+        application = new Application();
+        return 100; // 1s timer
+    }
+    void UserCPPCycle(uint32_t evtc, tUserCEvt *evtv)
+    {
+        application->Cycle(evtc, evtv);
+    }
+    void UserCPPTimer(void)
+    {
+        application->Timer();
+    }
+    void UserCPPDeInit(void)
+    {
+        delete application;
+    }
+}
